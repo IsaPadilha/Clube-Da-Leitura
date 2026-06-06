d43338fab277bb29bc1a63cc5d8ff9e361f84744
Isadora Padilha <isap4619@gmail.com>
2026-06-06 16:17:29 -0300

    Program MenuPrincipal 1 feito

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -1,8 +1,55 @@
 ﻿using ClubeDaLeitura.ConsoleApp.Compartilhado;
+using ClubeDaLeitura.ConsoleApp.ModuloCaixa;
 
+RepositorioCaixa repositorioCaixa = new RepositorioCaixa();
+TelaCaixa telaCaixa = new TelaCaixa(repositorioCaixa);
 
+TelaPrincipal telaPrincipal = new TelaPrincipal();
 
 while (true)
 {
+    string? opcaoMenuPrincipal = telaPrincipal.ObterMenuPrincipal();
 
+    if (opcaoMenuPrincipal == "S")
+    {
+        break;
+    }
+
+    while (true)
+    {
+        if (opcaoMenuPrincipal == "1") // caixas
+        {
+            string? opcaoMenuInterno = telaCaixa.ObterOpcaoMenu();
+
+            if (opcaoMenuInterno == "S")
+                break;
+
+            if (opcaoMenuInterno == "1")
+                telaCaixa.Cadastrar();
+
+            else if (opcaoMenuInterno == "2")
+                telaCaixa.Editar();
+
+            else if (opcaoMenuInterno == "3")
+                telaCaixa.Excluir();
+
+            else if (opcaoMenuInterno == "4")
+                telaCaixa.VisualizarTodos(true);
+        }
+
+        else if (opcaoMenuPrincipal == "2") // revistas
+        {
+
+        }
+
+        else if (opcaoMenuPrincipal == "3") // amigos
+        {
+
+        }
+
+        else if (opcaoMenuPrincipal == "4") // emprestimos
+        {
+
+        }
+    }
 }
