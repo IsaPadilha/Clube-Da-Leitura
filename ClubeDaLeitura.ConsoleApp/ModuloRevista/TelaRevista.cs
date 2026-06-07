f5910a4d01a6473e44e6f035f7824f35603f3bf2
Isadora Padilha <isap4619@gmail.com>
2026-06-07 17:20:06 -0300

    adicionada Edição de Revista

--- ClubeDaLeitura.ConsoleApp/ModuloRevista/TelaRevista.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloRevista/TelaRevista.cs
@@ -52,7 +52,31 @@ public class TelaRevista
 
     public void Editar()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Edição de Revista");
+        Console.WriteLine("---------------------------------");
+
+        // visualizar revistas cadastradas e obter o ID que deseja editar
+        VisualizarTodos(false);
+
+        Console.WriteLine("---------------------------------");
+
+        Console.Write("Digite o ID do registro que deseja editar: ");
+        int idSelecionado = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("---------------------------------");
+
+        // obter os novos dados da revista
+        Revista revistaAtualizada = ObterDadosCadastrais();
+
+        // editar a revista no repositório
+        repositorioRevista.Editar(idSelecionado, revistaAtualizada);
+
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine($"O registro \"{revistaAtualizada.Titulo}\" foi editado com sucesso!");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Digite ENTER para continuar");
+        Console.ReadLine();
     }
 
     public void Excluir()
